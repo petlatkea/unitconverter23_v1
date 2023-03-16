f505ec107ab66fcd482a272ec4af8bfd9fa14952
frva0001 <frva0001@gmail.com>
2023-03-16 14:38:35 +0100

    conversion functions
    inchToCm
    cmToInch

---
+++ conversions.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function inchToCm(inches) {
+   let centimeters = inches * 2.54;
+   return centimeters;
+}
+
+function cmToInch(centimeters) {
+   let inches = centimeters * 0.393701;
+   return inches;
+}
