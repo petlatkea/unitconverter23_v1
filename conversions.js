de35a39c343433c561d6dfca6919d7f64edbad83
JoeIbo1 <josef5222@hotmail.com>
2023-03-16 15:05:37 +0100

    done nr 8 Kenneth og Josef

---
+++ conversions.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function milesToKm(miles) {
+  const km = miles * 0.621371192;
+  return km;
+}
+
+function kmToMiles(kilometers) {
+  const miles = kilometers * 1.609344;
+  return miles;
+}
